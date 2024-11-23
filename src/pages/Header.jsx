1476e8fc0093eb3a13c78d3c5d0f7adc0552cd22
Mohammad-200 <mohammad.kizawi12@e-uvt.ro>
2024-11-23 14:27:15 +0200

    fix a bug in the a tag

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,50 +1,53 @@
-import React, { useRef, useState } from 'react'
-import navListData from '../data/navListData'
-import NavListItem from '../components/NavListItem'
+import React, { useRef, useState } from "react";
+import navListData from "../data/navListData";
+import NavListItem from "../components/NavListItem";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
-import './header.css'
+import "./header.css";
 
 function Header() {
   const [selectedTag, setSelectedTag] = useState(navListData);
   const navRef = useRef();
 
   const activateTag = (id) => {
-    setSelectedTag(prevTags => prevTags.map(tag => {
-      if (tag.id === id) {
-        return {...tag, active: true}
-      } else {
-        return {...tag, active: false}
-      }
-    }))
-  }
+    setSelectedTag((prevTags) =>
+      prevTags.map((tag) => {
+        if (tag.id === id) {
+          return { ...tag, active: true };
+        } else {
+          return { ...tag, active: false };
+        }
+      })
+    );
+  };
 
   const showNavBar = () => {
-
     navRef.current.classList.toggle("responsive_nav");
-  }
+  };
 
   return (
-    <header className='header-container'>
-        <a href='/' className='logo'>kiska.</a>
-        <ul className='nav' ref={navRef}>
-            {selectedTag.map(nav => {
-               return <NavListItem activateTag={activateTag}  key={nav.id} nav={nav}/>
-            })}
-            <button onClick={showNavBar} className='nav-btn nav-close-button'>
-               <IoMdClose  />
-            </button>
-        </ul>
-        <button onClick={showNavBar} className='nav-btn'>
-            <RxHamburgerMenu />
+    <header className="header-container">
+      <a href="/" className="logo">
+        kiska.
+      </a>
+      <ul className="nav" ref={navRef}>
+        {selectedTag.map((nav) => {
+          return (
+            <NavListItem activateTag={activateTag} key={nav.id} nav={nav} />
+          );
+        })}
+        <button onClick={showNavBar} className="nav-btn nav-close-button">
+          <IoMdClose />
         </button>
-        <div className="switchLanguages">
-          <a href='/'>EN</a>
-          |
-          <a href='/'>中文</a>
-        </div>
+      </ul>
+      <button onClick={showNavBar} className="nav-btn">
+        <RxHamburgerMenu />
+      </button>
+      <div className="switchLanguages">
+        <a href="#">EN</a>|<a href="#">中文</a>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
